2d077cf175289fdfb0b9617308567c0e47d67dcc
Gabriel Chagas <gabrielserq@outlook.com>
2023-09-22 23:00:24 -0300

    creating TestModule

--- src/prisma/prisma.service.spec.ts
+++ src/prisma/prisma.service.spec.ts
@@ -3,3 +3,4 @@ import { PrismaService } from './prisma.service';
 describe('PrismaService', () => {
   let service: PrismaService;
   beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
