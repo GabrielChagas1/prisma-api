8ef4fb67e0c14caa60df88732c4c2cad6a6ea6e6
Gabriel Chagas <gabrielserq@outlook.com>
2023-09-27 23:04:46 -0300

    creating and exporting class PrismaService

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,3 +1,4 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 @Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
