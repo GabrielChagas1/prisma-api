5725e03f739db8699d464a7ef52d3a1875682e5a
Gabriel Chagas <gabrielserq@outlook.com>
2023-09-28 23:05:11 -0300

    creating async function onModuleInit

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -2,3 +2,4 @@ import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
